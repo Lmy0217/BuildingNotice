00dac7bc8714f0346c472d04b666ac9f1b7ec5c3
Lmy0217 <lmy0217@126.com>
2019-07-02 12:32:08 +0800

    another up and down controller

--- src/main/java/org/cst/buildingnotice/web/FileUpAndDown.java
+++ src/main/java/org/cst/buildingnotice/web/FileUpAndDown.java
@@ -1,45 +1,82 @@
 package org.cst.buildingnotice.web;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Random;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 @Controller
 public class FileUpAndDown {
 
-//	public Result upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
-//        ServletContext sc = request.getSession().getServletContext();
-//        String dir = sc.getRealPath("/upload");
-//        String type = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1, file.getOriginalFilename().length());
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-//        Random r = new Random();
-//        String imgName = "";
-//        if ("jpg".equals(type)) {
-//            imgName = sdf.format(new Date()) + r.nextInt(100) + ".jpg";
-//        } else if ("png".equals(type)) {
-//            imgName = sdf.format(new Date()) + r.nextInt(100) + ".png";
-//        } else if ("jpeg".equals(type)) {
-//            imgName = sdf.format(new Date()) + r.nextInt(100) + ".jpeg";
-//        } else if ("gif".equals(type)) {
-//            imgName = sdf.format(new Date()) + r.nextInt(100) + ".gif";
-//        } else {
-//            return null;
-//        }
-//        try {
-//            FileUtils.writeByteArrayToFile(new File(dir, imgName), file.getBytes());
-//        } catch (IOException e) {
-//        	
-//        }
-//        return Result.ok().put("url", "/upload/" + imgName);
-//    }
+	@RequestMapping(value = "/fileUpLoad")
+	public String testUpload(@RequestParam("photo") CommonsMultipartFile file, @RequestParam("desc") String desc,
+			HttpServletRequest request) throws IOException {
+		ServletContext servletContext = request.getServletContext();// 获取ServletContext的对象 代表当前WEB应用
+		String realPath = servletContext.getRealPath("/uploads");// 得到文件上传目的位置的真实路径
+		System.out.println("realPath :" + realPath);
+		File file1 = new File(realPath);
+		if (!file1.exists()) {
+			file1.mkdir(); // 如果该目录不存在，就创建此抽象路径名指定的目录。
+		}
+		String prefix = UUID.randomUUID().toString();
+		prefix = prefix.replace("-", "");
+		String fileName = prefix + "_" + file.getOriginalFilename();// 使用UUID加前缀命名文件，防止名字重复被覆盖
+
+		InputStream in = file.getInputStream();
+		OutputStream out = new FileOutputStream(new File(realPath + "\\" + fileName));// 指定输出流的位置;
+
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		while ((len = in.read(buffer)) != -1) {
+			out.write(buffer, 0, len);
+			out.flush(); // 类似于文件复制，将文件存储到输入流，再通过输出流写入到上传位置
+		} // 这段代码也可以用IOUtils.copy(in, out)工具类的copy方法完成
+
+		out.close();
+		in.close();
+
+		return "success";
+	}
+
+	@RequestMapping("/fileDownLoad")
+	public ResponseEntity<byte[]> fileDownLoad(HttpServletRequest request) throws Exception {
+
+		ServletContext servletContext = request.getServletContext();
+		String fileName = "童话镇.mp3";
+		String realPath = servletContext.getRealPath("/WEB-INF/" + fileName);// 得到文件所在位置
+		InputStream in = new FileInputStream(new File(realPath));// 将该文件加入到输入流之中
+		byte[] body = null;
+		body = new byte[in.available()];// 返回下一次对此输入流调用的方法可以不受阻塞地从此输入流读取（或跳过）的估计剩余字节数
+		in.read(body);// 读入到输入流里面
+
+		fileName = new String(fileName.getBytes("gbk"), "iso8859-1");// 防止中文乱码
+		HttpHeaders headers = new HttpHeaders();// 设置响应头
+		headers.add("Content-Disposition", "attachment;filename=" + fileName);
+		HttpStatus statusCode = HttpStatus.OK;// 设置响应吗
+		ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(body, headers, statusCode);
+		return response;
+
+		// public ResponseEntity（T body，
+		// MultiValueMap < String，String > headers，
+		// HttpStatus statusCode）
+		// HttpEntity使用给定的正文，标题和状态代码创建一个新的。
+		// 参数：
+		// body - 实体机构
+		// headers - 实体头
+		// statusCode - 状态码
+	}
 }
