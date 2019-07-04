a65be537b2aa1f8ed5dfd31e829ddc1f57fb36ab
LuoV <rorwey@foxmail.com>
2019-07-04 15:52:16 +0800

    实现了比例计算以及传值

--- client/infoaAcquisition/js/form.js
+++ client/infoaAcquisition/js/form.js
@@ -2,18 +2,19 @@ function getForm(name) {
 	console.log(name);
 	//      console.log($('#form1').serializeJSON());
 	//      console.log(JSON.stringify($('#form1').serializeJSON()));
-	var froma=$(name).serializeJSON();
+	var froma = $(name).serializeJSON();
 	console.log(JSON.stringify(froma));
 	return (froma);
 }
 
-function computeBili(a,a_w){
-	var bili=0;
-	if(a!=0){
-		var bili=a_w/a;
+function computeBili(a, a_w) {
+	var bili = 0;
+	if (a != 0) {
+		var bili = a_w / a;
 	}
 	return bili;
 }
+
 function chuliForm(json) {
 	delete json["__view_array__"];
 	delete json["__IDENTITY__"];
@@ -23,3 +24,5 @@ function chuliForm(json) {
 	delete json["id"];
 	return json;
 }
+
+
