361bdb64de770c9c77c22a7b56bcefb2074acd5c
LuoV <rorwey@foxmail.com>
2019-07-05 15:54:33 +0800

    本地数据可以存入数据库了
    数据库数据，字段需要设计

--- client/infoaAcquisition/js/form.js
+++ client/infoaAcquisition/js/form.js
@@ -25,4 +25,62 @@ function chuliForm(json) {
 	return json;
 }
 
+// 关闭数据库
+function closeDB(){
+	console.log('关闭数据库: ');
+	plus.sqlite.closeDatabase({
+		name: 'test',
+		success: function(e){
+			console.log('closeDatabase success: '+JSON.stringify(e));
+		},
+		fail: function(e){
+			console.log('closeDatabase fail: '+JSON.stringify(e));
+		}
+	});
+}
+
+// 打开数据库
+function openDB(){
+	console.log('打开数据库: ');
+	plus.sqlite.openDatabase({
+		name: 'test',
+		path: '_doc/test.db',
+		success: function(e){
+			console.log('openDatabase success: '+JSON.stringify(e));
+		},
+		fail: function(e){
+			console.log('openDatabase success: '+JSON.stringify(e));
+		}
+	});
+}
+
+// 执行SQL语句
+function insertSQL(jsonInfo){
+	openDB();
+	console.log('执行SQL语句: ');
+	
+	sqlStr="insert into database values('"+jsonInfo.danwei+"','"+jsonInfo.floor+"','"+jsonInfo.result+"')"
+	plus.sqlite.executeSql({
+		name: 'test',
+		sql: 'create table if not exists database("danwei" CHAR(110),"floor" INT(2),"result" FLOAT(11))',
+		success: function(e){
+			console.log('executeSql success: '+JSON.stringify(e))
+			plus.sqlite.executeSql({
+				name: 'test',
+				sql: sqlStr,
+				success: function(e){
+					console.log('executeSql success: '+JSON.stringify(e))
+				},
+				fail: function(e){
+					console.log('executeSql fail: '+JSON.stringify(e))
+				}
+			})
+		},
+		fail: function(e){
+			console.log('executeSql fail: '+JSON.stringify(e))
+		}
+	});
+	closeDB()
+}
+
 
