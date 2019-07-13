eda71765c8ecd6a4982cb357d7d0e206e287bac1
Lmy0217 <lmy0217@126.com>
2019-07-13 20:43:29 +0800

    get imgs by archid

--- server/src/main/java/org/cst/buildingnotice/dao/ArchImgMapper.java
+++ server/src/main/java/org/cst/buildingnotice/dao/ArchImgMapper.java
@@ -1,9 +1,13 @@
 package org.cst.buildingnotice.dao;
 
+import java.util.List;
+
 import org.cst.buildingnotice.entity.ArchImg;
 
 public interface ArchImgMapper {
     int insert(ArchImg record);
 
     int insertSelective(ArchImg record);
+    
+    List<Integer> selectByArchid(int archid);
 }
---
+++ server/src/test/java/org/cst/buildingnotice/service/ArchImgServiceTest.java
@@ -0,0 +1,23 @@
+package org.cst.buildingnotice.service;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:applicationContext.xml"})
+public class ArchImgServiceTest {
+
+	@Autowired
+	private ArchImgService archImgService;
+	
+	@Test
+	public void getImgsByArchidTest() {
+		List<Integer> imgs = archImgService.getImgsByArchid(1);
+		System.out.println(imgs);
+	}
+}
