3b323882434529deb424389042c6404ba79b1a9c
Lmy0217 <lmy0217@126.com>
2019-07-13 20:45:35 +0800

    get type by id and get advise by id and body3

--- server/src/main/java/org/cst/buildingnotice/service/TypeService.java
+++ server/src/main/java/org/cst/buildingnotice/service/TypeService.java
@@ -1,13 +1,13 @@
 package org.cst.buildingnotice.service;
 
-import org.cst.buildingnotice.entity.Type;
+import org.cst.buildingnotice.entity.TypeWithBLOBs;
 
 public interface TypeService {
 
 	public int create(String name, String body1, String body2, String body3, 
 			String advise);
 
-	public Type getTypeById(int id);
+	public TypeWithBLOBs getTypeById(int id);
 	
-	public String getAdviseByIdAndBody3(int id, String body2);
+	public String getAdviseByIdAndBody3(int id, String body3);
 }
