e43c462fdd7a088fd5dbfbb5cc5eb31be351ed70
Lmy0217 <lmy0217@126.com>
2019-07-16 00:11:46 +0800

    commented service test code

--- server/src/test/java/org/cst/buildingnotice/service/ImageServiceTest.java
+++ server/src/test/java/org/cst/buildingnotice/service/ImageServiceTest.java
@@ -1,22 +1,22 @@
-package org.cst.buildingnotice.service;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:applicationContext.xml"})
-public class ImageServiceTest {
-	
-	@Autowired
-	public ImageService imageService;
-	
-	@Test
-	public void saveTest() {
-		//System.out.println(imageService.create("path1", "depict1"));
-		//System.out.println(imageService.create("path2", "depict2"));
-	}
-
-}
+//package org.cst.buildingnotice.service;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration({"classpath:applicationContext.xml"})
+//public class ImageServiceTest {
+//	
+//	@Autowired
+//	public ImageService imageService;
+//	
+//	//@Test
+//	public void saveTest() {
+//		System.out.println(imageService.create("path1", "depict1"));
+//		System.out.println(imageService.create("path2", "depict2"));
+//	}
+//
+//}
--- server/src/test/java/org/cst/buildingnotice/service/UserServiceTest.java
+++ server/src/test/java/org/cst/buildingnotice/service/UserServiceTest.java
@@ -1,22 +1,22 @@
-package org.cst.buildingnotice.service;
-
-import org.cst.buildingnotice.entity.User;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:applicationContext.xml"})
-public class UserServiceTest {
-	
-	@Autowired
-    public UserService userService;
-	
-	@Test
-    public void getUserByIdTest(){
-        User user = userService.getUserById(1);
-        System.out.println(user.getName());
-    }
-}
+//package org.cst.buildingnotice.service;
+//
+//import org.cst.buildingnotice.entity.User;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration({"classpath:applicationContext.xml"})
+//public class UserServiceTest {
+//	
+//	@Autowired
+//    public UserService userService;
+//	
+//	//@Test
+//    public void getUserByIdTest(){
+//        User user = userService.getUserById(1);
+//        System.out.println(user.getName());
+//    }
+//}
