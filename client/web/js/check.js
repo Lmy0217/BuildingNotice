60d7830640fe66a169fd26c6a7bbf7c26160ffb3
LuoV <rorwey@foxmail.com>
2019-07-19 11:40:38 +0800

    修复check不跳转问题

--- client/web/js/check.js
+++ client/web/js/check.js
@@ -3,48 +3,48 @@ console.log(document.cookie);
 console.log(q);
 var u = getCookie('user');
 console.log(u);
-window.onload = function() {
-	if (q == undefined) {
+// window.onload = function() {
+if (q == undefined) {
+	window.location.href = 'login.html'
+} else {
+	var c_start = document.cookie.indexOf("token");
+	//	console.log(c_start);
+	if (c_start == -1) {
 		window.location.href = 'login.html'
 	} else {
-		var c_start = document.cookie.indexOf("token");
-		//	console.log(c_start);
-		if (c_start == -1) {
-			window.location.href = 'login.html'
-		} else {
-			var data = {};
-			var data = transformToJson(data);
-			data["token"] = q;
-			//		console.log(data);
-			$.ajax({
-				url: checkUser,
-				cache: false,
-				type: "post",
-				datatype: "json",
-				contentType: 'application/json;charset=UTF-8',
-				data: JSON.stringify(data),
-				success: function(data) {
-					console.log(data);
-					if (data.status == "200") {
-						//根据后台返回值确定是否操作成功
-						console.log("验证成功");
-						if (u != undefined) {
-							$("#user").append(u);
-							$("#user").val(u);
-						} else {
-							$("#user").append("管理员");
-						}
-						document.cookie = setCookie("token", data.token, "3");
+		var data = {};
+		var data = transformToJson(data);
+		data["token"] = q;
+		//		console.log(data);
+		$.ajax({
+			url: checkUser,
+			cache: false,
+			type: "post",
+			datatype: "json",
+			contentType: 'application/json;charset=UTF-8',
+			data: JSON.stringify(data),
+			success: function(data) {
+				console.log(data);
+				if (data.status == "200") {
+					//根据后台返回值确定是否操作成功
+					console.log("验证成功");
+					if (u != undefined) {
+						$("#user").append(u);
+						$("#user").val(u);
 					} else {
-						console.log("非法访问");
-						window.location.href = 'login.html'
+						$("#user").append("管理员");
 					}
+					document.cookie = setCookie("token", data.token, "3");
+				} else {
+					console.log("非法访问");
+					window.location.href = 'login.html'
 				}
-			});
-		}
-
+			}
+		});
 	}
+
 }
+// }
 
 //获取cookie
 function getCookie(cname) {
