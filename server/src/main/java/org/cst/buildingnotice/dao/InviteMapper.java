4e2c130613a471063bb42cec530d4d41c9918813
Lmy0217 <lmy0217@126.com>
2019-07-30 23:33:27 +0800

    insert invite list

--- server/src/main/java/org/cst/buildingnotice/dao/InviteMapper.java
+++ server/src/main/java/org/cst/buildingnotice/dao/InviteMapper.java
@@ -1,11 +1,16 @@
 package org.cst.buildingnotice.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.cst.buildingnotice.entity.Invite;
 
 public interface InviteMapper {
     int deleteByPrimaryKey(String code);
 
     int insert(Invite record);
+    
+    int insertCodes(@Param("codes") List<String> codes, @Param("createid") Integer createid);
 
     int insertSelective(Invite record);
 
