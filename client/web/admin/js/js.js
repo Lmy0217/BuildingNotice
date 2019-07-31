acac90766a97ebd40788983316c180ef8b39cb18
rorwey <rorwey@foxmail.com>
2019-07-31 08:17:22 +0800

    Update js.js

--- client/web/admin/js/js.js
+++ client/web/admin/js/js.js
@@ -334,7 +334,7 @@ function getChkValue(chk){
  
  	for (var i = 1; i <= 3; i++) {
  		if (page - i > 1) {
- 			str = '<a href='+url+'?type='+type+'page='+(page - i) +'>' + (page - i) + '</a> ' + str;
+ 			str = '<a href='+url+'?type='+type+'&page='+(page - i) +'>' + (page - i) + '</a> ' + str;
  		}
  		if (page + i < total) {
  			str = str + ' ' +'<a href='+url+'?type='+type+'page='+(page +  i) +'>'+ (page + i)+ '</a> ';
@@ -346,7 +346,7 @@ function getChkValue(chk){
  	}
  
  	if (page > 1) {
- 		str = '<a href='+url+'?type='+type+'page='+(page - 1) +'>' +'<上一页 '+'</a> ' + 
+ 		str = '<a href='+url+'?type='+type+'&page='+(page - 1) +'>' +'<上一页 '+'</a> ' + 
  		'<a href='+url+'?type='+type+'page='+1 +'>'+ 1 + '</a> '  + str;
  	}
  
@@ -355,7 +355,7 @@ function getChkValue(chk){
  	}
  
  	if (page < total) {
- 		str = str + ' ' + '<a href='+url+'?type='+type+'page='+(total) +'>' +total+ '</a> '+ 
+ 		str = str + ' ' + '<a href='+url+'?type='+type+'&page='+(total) +'>' +total+ '</a> '+ 
  		'<a href='+url+'?type='+type+'page='+(page +  1) +'>'+' 下一页 >'+ '</a> ';
  	}
  
