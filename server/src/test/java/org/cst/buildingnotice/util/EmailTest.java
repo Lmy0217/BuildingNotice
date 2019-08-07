6ace767b02bc0c780ba9907beb1e98da9e171b6c
Lmy0217 <lmy0217@126.com>
2019-08-08 00:35:03 +0800

    note test code

--- server/src/test/java/org/cst/buildingnotice/util/EmailTest.java
+++ server/src/test/java/org/cst/buildingnotice/util/EmailTest.java
@@ -1,17 +1,17 @@
-package org.cst.buildingnotice.util;
-
-import org.cst.buildingnotice.config.Config;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:applicationContext.xml"})
-public class EmailTest {
-
-	@Test
-	public void sendEmailTest() {
-		EmailUtil.sendVerify("lmy0217@126.com", "myluo", Config.ROOT_HOST);
-	}
-}
+//package org.cst.buildingnotice.util;
+//
+//import org.cst.buildingnotice.config.Config;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration({"classpath:applicationContext.xml"})
+//public class EmailTest {
+//
+//	@Test
+//	public void sendEmailTest() {
+//		EmailUtil.sendVerify("lmy0217@126.com", "myluo", Config.ROOT_HOST);
+//	}
+//}
